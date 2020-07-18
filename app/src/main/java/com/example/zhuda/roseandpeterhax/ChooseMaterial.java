a164994cec5c17781cde8d510fc2d6987a1d37f0
Peter Zhu <zhudanmeng@gmail.com>
2020-07-18 17:50:48 -0300

    AddefW3R2

---
+++ app/src/main/java/com/example/zhuda/roseandpeterhax/ChooseMaterial.java
@@ -0,0 +1,13 @@
+package com.example.zhuda.roseandpeterhax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseMaterial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_material);
+    }
+}
--- app/src/main/java/com/example/zhuda/roseandpeterhax/MainActivity.java
+++ app/src/main/java/com/example/zhuda/roseandpeterhax/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.zhuda.roseandpeterhax;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initiate and perform click event on buttons
+        ImageButton tshirt = (ImageButton)findViewById(R.id.tshirt);
+        tshirt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChooseMaterial.class));
+            }
+        });
+
+
     }
+
 
 
 }
